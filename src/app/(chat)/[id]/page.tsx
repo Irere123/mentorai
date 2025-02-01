691171d532142dd229729f0b35c6feb230542271
irere123 <irere2020@outlook.com>
2025-02-01 16:36:53 +0200

    feat: ai chat and files modal

--- src/app/(chat)/[id]/page.tsx
+++ src/app/(chat)/[id]/page.tsx
@@ -1,7 +1,34 @@
-export default async function Page() {
+import { Message } from "ai";
+import { Chat } from "@/db/schema";
+import { getChatById } from "@/db";
+import { notFound } from "next/navigation";
+import { Chat as PreviewChat } from "@/components/chat";
+import { auth } from "@/app/(auth)/auth";
+
+export default async function Page({ params }: { params: any }) {
+  const { id } = params;
+  const chatFromDb = await getChatById({ id });
+
+  if (!chatFromDb) {
+    notFound();
+  }
+
+  const chat: Chat = {
+    ...chatFromDb,
+    messages: chatFromDb.messages as Message[],
+  };
+
+  const session = await auth();
+
+  if (chat.author !== session?.user?.email) {
+    notFound();
+  }
+
   return (
-    <div>
-      <p>Hello </p>
-    </div>
+    <PreviewChat
+      id={chat.id}
+      initialMessages={chat.messages}
+      session={session}
+    />
   );
 }
